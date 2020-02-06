2ec975a67db077110c681479a977edd6a9ef3962
promahmudul <mahmudul.coder@gmail.com>
2020-02-06 09:54:11 +0600

    Posts process of blog homepage according to post format

--- functions.php
+++ functions.php
@@ -18,6 +18,7 @@ function philosophy_theme_setup() {
 	add_editor_style( '/assets/css/editor-style.css' );
 
 	register_nav_menu( 'topmenu', __('Top Menu', 'philosophy'));
+	add_image_size('philosopy-home-square', 400, 400, true);
 }
 add_action( 'after_setup_theme', 'philosophy_theme_setup' );
 
